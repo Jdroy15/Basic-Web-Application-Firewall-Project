7536eb5032c362daf86adda08562d28fb480133b
Jdroy15 <84408149+Jdroy15@users.noreply.github.com>
2025-05-07 15:37:03 +0200

    Create app_to_protect.py

---
+++ app_to_protect.py
@@ -0,0 +1,14 @@
+#here it can be the code of Frontend of the Project.
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Protected App: Home Page"
+
+@app.route('/login', methods=['POST'])
+def login():
+    return "Login endpoint reached"
+
+if __name__ == '__main__':
+    app.run(port=5000)
